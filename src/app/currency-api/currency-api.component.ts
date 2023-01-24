6a6c6f227b6e238ed929c66542db61db7532c2ce
Oleg Fominyh <fominyh.oleg@gmail.com>
2023-01-24 13:51:37 +0300

    error handling added

--- src/app/currency-api/currency-api.component.ts
+++ src/app/currency-api/currency-api.component.ts
@@ -9,15 +9,15 @@ export class CurrencyApiComponent implements OnInit {
   public currency: ICurrency = null;
 
   constructor(private datasource: CurrencyApiDatasourceService) { }
-  //public errorMessage: string = ''; //сюда запишется текст ошибки перехваченной в dataservice
-  public errorMessage: any = null; // сюда записываем объект-ошибку перехваченную из subscribe 
+  //public errorMessage: string = '';   //сюда запишется текст ошибки перехваченной в dataservice
+  public errorMessage: any = null;  // сюда записываем объект-ошибку перехваченную из subscribe 
   ngOnInit() {
     this.getInr();
     //this.errorMessage = this.datasource.errorMessage
   }
 
   public getInr(): void {
-    console.log('before getInr: ' + "errorMessage")
+    //console.log('before getInr: ' + "errorMessage")
     this.datasource.getInr().subscribe ({
       next: (data: any) => {
         this.currency = data[this.getRandomInt(1, 300)] 
@@ -25,10 +25,10 @@ export class CurrencyApiComponent implements OnInit {
       error: (error: any) => {
         //this.errorMessage = this.datasource.errorMessage; //используется, если в datasource не закомменчена обработка ошибок
         this.errorMessage = error; 
-        console.log('this is currency-api and error message is: ' + this.errorMessage)
+        console.log(this.errorMessage)
       }
     })
-    console.log('after getInr: ' + "errorMessage") //выполняется сразу, не дожидаясь ответа по subscribe
+    //console.log('after getInr: ' + "errorMessage")   //выполняется сразу, не дожидаясь ответа по subscribe
   }
 
   private getRandomInt(min: number, max: number): number {
--- src/app/info-card/info-card.component.ts
+++ src/app/info-card/info-card.component.ts
@@ -14,14 +14,15 @@ export class InfoCardComponent implements OnInit, OnChanges {
   //@Input() errMessage: string = '';
   @Input() errMessage: any = null;
 
-  public messageItself: string = '';
-  //public condition: boolean = true; // лишнее действие, привязка автообновляется итак при любых изменениях
+  public errorMessage: string = '';
+  public errorDescription: string = '';
+  public errorCode: string = '';
+  //public condition: boolean = true;  // лишнее действие, привязка автообновляется итак при любых изменениях
   
   ngOnInit() {
     this.SetClasses(this.cardType)
-    console.log('this is info-card (1 OnInit) and error message is: ' + this.errMessage) // это действие выполняется до того как получен ответ по subscribe
-    //this.messageItself = this.errMessage.error.message
-    this.messageItself = this.errMessage?.error.message
+    //console.log('this is info-card (1 OnInit) and error message is: ' + this.errMessage)   // это действие выполняется до того как получен ответ по subscribe
+    //this.messageItself = this.errMessage?.error.message  // здесь не вызывается по новой
   }
   ngOnChanges(changes: SimpleChanges) {
     /*for (let propName in changes) {
@@ -30,9 +31,12 @@ export class InfoCardComponent implements OnInit, OnChanges {
       let prev = JSON.stringify(chng.previousValue);
       console.log(`we have changes in info-card: ${propName}: currentValue = ${cur}, previousValue = ${prev}`);
     }*/
-    //console.log('this is info-card (2 OnChanges) and error message is: ' + this.errMessage) // это действие выполняется до того как получен ответ по subscribe
-    //this.condition = !this.errMessage // лишнее действие, привязка автообновляется итак при любых изменениях
+    //console.log('this is info-card (2 OnChanges) and error message is: ' + this.errMessage)   // это действие выполняется до того как получен ответ по subscribe
+    //this.condition = !this.errMessage   // лишнее действие, привязка автообновляется итак при любых изменениях
     //console.log('this is info-card. Condition : ' + this.condition)
+    this.errorMessage = this.errMessage?.message
+    this.errorDescription = this.errMessage?.error.message
+    this.errorCode = this.errMessage?.status
   }
 
   currentClasses: any = null;
